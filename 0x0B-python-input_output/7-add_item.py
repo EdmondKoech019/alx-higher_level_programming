f4fa54289d017663fc8cb2d0f56863425c92e809
Edmund <60813896+EdmondKoech019@users.noreply.github.com>
2022-09-27 15:47:41 -0700

    Add files via upload

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""Write a script that adds all arguments to a Python list, and then save
-   them to a file:
-        *If the file doesnt exist, it should be created
 """
-from sys import argv
-from os import path
+Module with script that adds all arguments to  a Python list,
+and then save them to a file
+"""
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
+
 try:
-    my_items = load_from_json_file(filename)
+    lst = load_from_json_file(filename)
+except Exception:
+    lst = []
 
-except FileNotFoundError:
-    my_items = []
-for i in range(1, len(argv)):
-    my_items.append(argv[i])
-
-save_to_json_file(my_items, filename)
+for i in range(1, len(sys.argv)):
+    lst.append(sys.argv[i])
+    save_to_json_file(lst, filename)
