c4856bbce588b184705b4cb3c26f60aafbd00ded
Edmund <60813896+EdmondKoech019@users.noreply.github.com>
2022-12-17 01:51:02 +0300

    Add files via upload

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 """
-a Python script that takes in a URL, sends a request to the URL and displays the value of
-the X-Request-Id variable found in the header of the response.
+script that takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id variable
+found in the header of the response
 """
 import sys
-from urllib import response
 import urllib.request
-
-
 if __name__ == "__main__":
-     url = sys.argv[1]
-
-     requestData = urllib.request.Request(url)
-     with urllib.request.urlopen(requestData) as responseData:
-         print(dict(responseData.headers).get("X-Request-Id"))
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.headers.get("X-Request-Id"))
--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """
-takes in a URL, sends a request to the URL and displays the value of the
-variable X-Request-Id in the response header
+script that takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id variable
+found in the header of the response
 """
-if __name__ == '__main__':
-    import requests
-    from sys import argv
-    r = requests.get(argv[1])
-    print(r.headers.get('X-Request-Id'))
+import sys
+import requests
+if __name__ == "__main__":
+    response = requests.get(sys.argv[1])
+    print(response.headers.get("X-Request-Id"))
